999c9baffe217e5421687365c16c4fe7a3767966
unknown <jantimar2@gmail.com>
2013-06-21 16:24:18 +0200

    commit

---
+++ PuzzleGame/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PuzzleGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PuzzleGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext database = new DataContext();
+
+        public String Index()
+        {
+            return "Server run...";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AllScores()
+        {
+            return Json(database.Score.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddScore(String Name, int Score)
+        {
+            database.Score.Add(new TabelScore { Name = Name, Score = Score});
+            database.SaveChanges();
+            return Json(database.Score.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ PuzzleGame/Models/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PuzzleGame.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<TabelScore> Score { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Vytvorenie databazy ak neexistuje
+            Database.SetInitializer<DataContext>(new CreateDatabaseIfNotExists<DataContext>());
+        }
+    }
+
+    [Table("Scores")]
+    public class TabelScore
+    {
+        [Key]
+        public int Score_id { get; set; }
+        [Column("Player"), StringLength(50)]
+        public String Name { get; set; }
+        [Column ("Score")]
+        public int Score { get; set; }
+    }
+
+}
